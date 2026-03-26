897fe7a9ba1a305831b00e72caa4e82dc6d45350
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-03-26 12:05:07 +0530

    Create Floyd's Triangle Pattern.cpp

---
+++ Floyd's Triangle Pattern.cpp
@@ -0,0 +1,15 @@
+// Floyd's Triangle Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int temp = 1;
+    for(int i = 1; i<=n;i++){
+        for(int j=0;j<i;j++){
+            cout<<temp;
+            temp++;
+        }
+        cout<<endl;
+    }
+}
