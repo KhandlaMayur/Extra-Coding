b6259a2b5391f32bd12c0d69e74c6025dfb83fae
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-03-26 12:00:32 +0530

    Create Inverted Triangle Pattern.cpp

---
+++ Inverted Triangle Pattern.cpp
@@ -0,0 +1,16 @@
+// Inverted Triangle Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n;i++){
+        for(int j=0;j<i-1;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<=n-i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+}
