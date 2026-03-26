5ce779562d563ef9b949eb2f05e77132322cabe7
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-03-26 12:35:26 +0530

    Create Square Pattern.cpp

---
+++ Square Pattern.cpp
@@ -0,0 +1,19 @@
+// Square Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int temp = 1;
+    for(int i = 1; i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cout<<temp;
+            temp++;
+        }
+        cout<<endl;
+    }
+}
+
+// 123
+// 456
+// 789
