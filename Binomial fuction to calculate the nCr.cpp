2caebd3dd700fc1cfff5deca287e03dc6b4ec233
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-03-26 15:40:32 +0530

    Create Binomial fuction to calculate the nCr.cpp

---
+++ Binomial fuction to calculate the nCr.cpp
@@ -0,0 +1,25 @@
+// Binomial fuction to calculate the nCr
+#include<iostream>
+using namespace std;
+
+int factorial(int n){
+    int fact = 1;
+    for(int i=1; i<=n; i++){
+        fact *= i;
+    }
+    return fact;
+}
+
+int ncr(int n,int r){
+    int fact_n=factorial(n);
+    int fact_r=factorial(r);
+    int fact_nmr=factorial(n-r);
+    return fact_n / (fact_r * fact_nmr);
+}
+
+int main(){
+    int n,r;
+    cin>>n;
+    cin>>r;
+    cout << ncr(n,r);
+}
