e2097d40684e62e434ea611b06ce3ff567f1b001
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-03-26 12:18:12 +0530

    Create Triangle Pattern.cpp

---
+++ Triangle Pattern.cpp
@@ -0,0 +1,18 @@
+// Triangle Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
+
+// 1
+// 12
+// 123
+// 1234
