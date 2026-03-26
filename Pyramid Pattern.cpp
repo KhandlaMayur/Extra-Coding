139c4598c1b144e4084f0808d07b8300dc163f89
Mayur Khandla <167976601+KhandlaMayur@users.noreply.github.com>
2026-03-26 12:45:06 +0530

    Create Pyramid Pattern.cpp

---
+++ Pyramid Pattern.cpp
@@ -0,0 +1,25 @@
+// Pyramid Pattern
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 1; i<=n;i++){
+        for(int j=0;j<n-i;j++){
+            cout<<" ";
+        }
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        for(int j=1;j<=i-1;j++){
+            cout<<j;
+        }
+        
+        cout<<endl;
+    }
+}
+
+//    1
+//   121
+//  12312
+// 1234123
